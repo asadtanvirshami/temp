3f825d82220882d32b16bc5421ea41fc417ef411
asVD <asadworkemail@gmail.com>
2024-08-09 16:37:10 -0700

    commit

--- components/NavItem/index.tsx
+++ components/NavItem/index.tsx
@@ -7,13 +7,13 @@ interface INavItemProps {
   Icon: React.ReactNode;
   label?: string;
   active?: boolean;
-  href?:string;
+  href: string;
 }
 
 const NavItem = ({ Icon, label, active, href }: INavItemProps) => {
   return (
     <Link
-      href={href}
+      href={href || "#"}
       className={`flex hover:bg-[#2D31FA] pt-[20px] px-4 h-[47px] rounded-b-lg gap-2 transition duration-300 ease-in-out ${
         active && "bg-[#2D31FA]"
       }`}
