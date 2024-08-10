f4230cd62c01dc65543fe4515ec33f303ec60020
asVD <asadworkemail@gmail.com>
2024-08-09 17:04:17 -0700

    v

--- app/api/invoice/towmast/route.ts
+++ app/api/invoice/towmast/route.ts
@@ -1,18 +1,18 @@
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
 
-import { fetchFromApi } from "@/lib/appClient";
+// import { fetchFromApi } from "@/lib/appClient";
 
-export async function POST(request: NextRequest) {
+// export async function POST(request: NextRequest) {
 
-    const page = await request.json()
+//     const page = await request.json()
 
-    const apiUrl = `http://localhost:5000/api/towmast?limit=1&&page=${Number(page)}&sort=-dispnum`;
+//     const apiUrl = `http://localhost:5000/api/towmast?limit=1&&page=${Number(page)}&sort=-dispnum`;
 
-    const invoiceResponse = await fetchFromApi(apiUrl);
+//     const invoiceResponse = await fetchFromApi(apiUrl);
 
-    if ("error" in invoiceResponse) {
-        return NextResponse.json(invoiceResponse, { status: 400 });
-    }
+//     if ("error" in invoiceResponse) {
+//         return NextResponse.json(invoiceResponse, { status: 400 });
+//     }
 
-    return NextResponse.json(invoiceResponse.data, { status: invoiceResponse.status });
-}
+//     return NextResponse.json(invoiceResponse.data, { status: invoiceResponse.status });
+// }
--- app/api/truck/route.ts
+++ app/api/truck/route.ts
@@ -1,23 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
-    const options: RequestInit = {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    };
+// export async function GET(request: NextRequest) {
+ 
+//     if ("error" in trucksResponse) {
+//         // Bad request
+//         return NextResponse.json(trucksResponse, { status: 400 });
+//     }
 
-    const trucksResponse = await fetch(
-        `http://localhost:5000/api/trucks/`,
-        options,
-    ).then((res) => res.json());
-
-    // console.log(driverResponse)
-    if ("error" in trucksResponse) {
-        // Bad request
-        return NextResponse.json(trucksResponse, { status: 400 });
-    }
-
-    return NextResponse.json(trucksResponse, { status: 200 });
-}
+//     return NextResponse.json(trucksResponse, { status: 200 });
+// }
